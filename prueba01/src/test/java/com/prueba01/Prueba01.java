f36e822d9f8e25d42146726360990b56dbda05a2
ChrisNVA <chrisnvam@gmail.com>
2020-04-16 18:07:32 -0500

    primer commit de prueba para jenkins

--- prueba01/src/test/java/com/prueba01/Prueba01.java
+++
@@ -1,8 +0,0 @@
-package com.prueba01;
-
-public class Prueba01 {
-	public static void main(String[] args) {
-		System.out.println("Prueba 01");
-		//prueba001
-	}
-}
---
+++ prueba01/src/test/java/com/prueba01/Prueba01_Test.java
@@ -0,0 +1,10 @@
+package com.prueba01;
+
+import org.testng.annotations.Test;
+
+public class Prueba01_Test {
+	@Test
+	public void prueba01() {
+		System.out.println("------->>>>>>>>>>              Prueba 001             <<<<<<<<<<<<<<<<------------------");
+	}
+}
